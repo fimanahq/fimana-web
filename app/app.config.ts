9f9d63d3e817731ff8050d9c650f285a6c3ef0d6
joelvergdev <joelaquinovergara@gmail.com>
2026-01-28 16:20:24 +0800

    Update primary color to blue and replace AppLogo icon

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,9 +1,9 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'green',
+      primary: 'blue',
       secondary: 'orange',
-      neutral: 'zinc'
+      neutral: 'slate'
     },
     input: {
       defaultVariants: {
